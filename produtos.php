8ef322c68eabb47fd388418a451c9523bd6126e8
Josue Wendell <101228263+josue7522@users.noreply.github.com>
2023-05-03 21:48:07 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" /><title>Document</title>
+</head>
+<body>
+    <div class="v7_3">
+        <div class="v1_2">
+            <div class="v1_5">
+
+            </div>
+            <span class="facalogin">Olá! faça seu login ou cadastre-se</span>
+            <div class="v1_7"></div>
+            <div class="v1_8"></div>
+            <span class="v1_9">TOTAL METALCOINS</span>
+            <span class="produto-a-ser-trocado">PRODUTOS A SER TROCADOS</span>
+            <div class="v2_24"></div>
+            <span class="home">home</span>
+            <span class="o_que_metalcoins">o que e metalcoin?</span>
+            <span class="contatos">contatos</span>
+            <span class="inicio_loja">INICIO / LOJA</span>
+            <div class="fundoverde_1"></div>
+            <span class="letratrocar_1">TROCAR</span>
+            <div class="fundoverde_2"></div>
+            <span class="letratrocar_2">TROCAR</span>
+            <div class="fundoverde_3"></div>
+            <span class="letratrocar_3">TROCAR</span>
+
+
+
+
+            <span class="chuteira">Chuteiras</span>
+            <span class="camisetas">Camisetas</span>
+            <div class="v2_52">Meia</div>
+            <span class="v2_53">Caneleira</span>
+            <span class="v2_55">Short</span>
+            <img id="chuteirola" src="img/v4_11.png" alt="">
+            <span class="v2_56">Acessorios</span>
+            <img id="chuteirola1" src="img/v4_11.png" alt="">
+            <img id="chuteirola2" src="img/v4_11.png" alt="">
+            <img id="chuteirola3" src="img/v4_11.png" alt="">
+            <img id="chuteirola4" src="img/v4_11.png" alt="">
+            <div class="luzverde"></div>
+
+
+
+            <input class="barrapesquisa">O que você esta procurando hoje ?</input>
+            <span class="Todos">Todos</span>
+            <span class="coin1">500metalcoins</span>
+            <span class="coin2">500metalcoins</span>
+            <span class="coin3">800metalcoins</span>
+            <span class="coin4">800metalcoins</span>
+            <span class="coin8">320metalcoins</span>
+            <span class="item1">CHUTEIRA ROSA DA NIKE COM SOLA DE CRAVO</span>
+            <span class="item2">CHUTEIRA LARANJA COM SOLADE CRAVO</span>
+            <span class="item3">CHUTEIRA PRETA COM SOLA DE CRAVO</span>
+            <span class="item4">CHUTEIRA PRETA COM SOLA DE CRAVO</span>
+            <img id="logovix" src="img/v7_2.png" alt="">
+            <img id="logologin" src="img/v2_2.png" alt="">
+            <span class="logovix"></div>
+            
+
+
+
+            <div>
+                <input class="fundoverde_1" type="button" value="TROCAR">
+                <input class="fundoverde_2" type="button" value="TROCAR">
+                <input class="fundoverde_3" type="button" value="TROCAR">
+                <input class="fundoverde_4" type="button" value="TROCAR">
+                <input class="fundoverde_5" type="button" value="TROCAR">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
